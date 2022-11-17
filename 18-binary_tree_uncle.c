5a9c6f0732c26dc00bf08b0101e4cdbfe8830d2f
Getachew Alemayehu <105641499+ge1351@users.noreply.github.com>
2022-11-17 22:58:07 +0300

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,18 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - function that finds if a node is sibling
- * @node: node to check
- * Return: The uncle node
+ * binary_tree_uncle - function that finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ *
+ * Return: must return a pointer to the uncle node, otherwise NULL
  */
-
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-	{
+	binary_tree_t *aux;
+
+	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
+	aux = node->parent->parent;
+	node = node->parent;
+	if (aux->left == node)
+	{
+		if (!aux->right)
+			return (NULL);
+		return (aux->right);
 	}
-	if (node->parent->parent->left == node->parent)
-		return (node->parent->parent->right);
-	return (node->parent->parent->left);
+	else
+	{
+		if (!aux->left)
+			return (NULL);
+		return (aux->left);
+	}
 }
