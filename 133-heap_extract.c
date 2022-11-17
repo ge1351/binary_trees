00dc1997b503c45306d4556a7b221abcbcb1c521
Getachew Alemayehu <105641499+ge1351@users.noreply.github.com>
2022-11-18 00:30:40 +0300

    Update 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
+
 /**
- * heap_extract - ?
- * @root: ?
- * Return: ?
+ * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of integers
+ *
+ * @root: double pointer to the root node of heap
+ * Return: value stored in the root node
  */
 int heap_extract(heap_t **root)
 {
-	(void) root;
-	return (0);
+	return (root);
 }
