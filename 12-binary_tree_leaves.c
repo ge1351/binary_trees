4dfc1fb334169604c453caa5db22c6bf1e9a27f4
Getachew Alemayehu <105641499+ge1351@users.noreply.github.com>
2022-11-17 21:02:23 +0300

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,42 +1,22 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_leaves - Counts the leaves in a binary tree
- * @tree : a pointer to the root node of the binary tree
- *
- * Return: the quantity of leaves in the binary tree
+ * binary_tree_leaves - function that returns the number of leaves in a tree
+ * @tree: tree to check
+ * Return: number of leaves
  */
-
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t qty_leaves = 0;
+	size_t leaf = 0, l = 0, r = 0;
 
-	if (!tree)
+	if (tree == NULL)
+	{
 		return (0);
-	return (rec_bin_tree_leaves(qty_leaves, tree));
-}
-
-/**
- * rec_bin_tree_leaves - Calculates the quantity of leaves of a binary tree
- * @qty_leaves : quantity of leaves
- * @tree : a pointer to the node of the binary tree
- *
- * Return: the quantity of leaves
- */
-
-size_t rec_bin_tree_leaves(size_t qty_leaves, const binary_tree_t *tree)
-{
-	size_t left = 0, right = 0, result = 0;
-
-	if (tree->left)
-		left = rec_bin_tree_leaves(qty_leaves, tree->left);
-	if (tree->right)
-		right = rec_bin_tree_leaves(qty_leaves, tree->right);
-
-	result = left + right;
-
-	if (!tree->left && !tree->right)
-		result++;
-
-	return (result);
+	}
+	else
+	{
+		l = binary_tree_leaves(tree->left);
+		r = binary_tree_leaves(tree->right);
+		leaf = l + r;
+		return ((!l && !r) ? leaf + 1 : leaf + 0);
+	}
 }
